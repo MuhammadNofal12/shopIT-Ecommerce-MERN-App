e29db3824ea76473147e8b2cbe691e3ccc137ceb
Muhammad Nofal12 <muhammadnofal446@gmail.com>
2026-02-25 14:19:43 +0500

    fixed database issue

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -29,11 +29,13 @@ import mongoose from "mongoose";
 export const connectDatabase = async () => {
   try {
     let DB_URI;
-    const env = process.env.NODE_ENV?.toUpperCase();
 
-    if (env === "DEVELOPMENT") {
+    const env = process.env.NODE_ENV;
+
+    if (env === "development") {
       DB_URI = process.env.DB_LOCAL_URI;
-    } else if (env === "production") {
+    } else {
+      // production or anything else
       DB_URI = process.env.DB_URI;
     }
 
@@ -41,10 +43,9 @@ export const connectDatabase = async () => {
       throw new Error("Database URI is missing. Check env variables.");
     }
 
-    // Connect to MongoDB
     const con = await mongoose.connect(DB_URI, {
-      family: 4, // Forces IPv4 (fix for Windows sometimes)
-      serverSelectionTimeoutMS: 30000, // Wait 30s before throwing timeout
+      family: 4,
+      serverSelectionTimeoutMS: 30000,
     });
 
     console.log(`MongoDB connected with HOST: ${con.connection.host}`);
@@ -53,7 +54,6 @@ export const connectDatabase = async () => {
     process.exit(1);
   }
 };
-
 // import mongoose from "mongoose";
 
 // export const connectDatabase = async () => {
