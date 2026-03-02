8e175d1338a2c2db6851b83f9404d31caadfc371
Muhammad Nofal12 <muhammadnofal446@gmail.com>
2026-03-02 15:15:36 +0500

    redo chnges

--- frontend/src/components/cart/ConfirmOrder.jsx
+++ frontend/src/components/cart/ConfirmOrder.jsx
@@ -1,180 +1,22 @@
-// import React from "react";
-// import MetaData from "../layout/MetaData";
-// import { useSelector } from "react-redux";
-// import { Link } from "react-router-dom";
-// import { calculateOrderCost } from "../../helpers/helpers";
-// import CheckoutSteps from "./CheckoutSteps";
-
-// const ConfirmOrder = () => {
-//   const { cartItems, shippingInfo } = useSelector((state) => state.cart);
-//   const { user } = useSelector((state) => state.auth);
-
-//   //const navigate = useNavigate();
-
-//   const { itemsPrice, shippingPrice, taxPrice, totalPrice } =
-//     calculateOrderCost(cartItems);
-
-//   //   const proceedToPaymentHandler = () => {
-//   //     navigate("/payment_method");
-//   //   };
-//   return (
-//     <>
-//       <MetaData title={"Confirm Order Info"} />
-//       <CheckoutSteps shipping confirmOrder />
-
-//       <div className="row d-flex justify-content-between">
-//         <div className="col-12 col-lg-8 mt-5 order-confirm">
-//           <h4 className="mb-3">Shipping Info</h4>
-//           <p>
-//             <b>Name:</b> {user?.name}
-//           </p>
-//           <p>
-//             <b>Phone:</b> {shippingInfo?.phoneNo}
-//           </p>
-//           <p className="mb-4">
-//             <b>Address:</b> {shippingInfo?.address}, {shippingInfo?.city},
-//             {shippingInfo?.zipCode}, {shippingInfo?.country}
-//           </p>
-
-//           <hr />
-//           <h4 className="mt-4">Your Cart Items:</h4>
-//           {cartItems?.map((item) => (
-//             <>
-//               <hr />
-//               <div className="cart-item my-1">
-//                 <div className="row">
-//                   <div className="col-4 col-lg-2">
-//                     <img
-//                       src={item?.image}
-//                       alt="Laptop"
-//                       height="45"
-//                       width="65"
-//                     />
-//                   </div>
-
-//                   <div className="col-5 col-lg-6">
-//                     <Link to={`/product/${item.product}`}>{item?.name}</Link>
-//                   </div>
-
-//                   <div className="col-4 col-lg-4 mt-4 mt-lg-0">
-//                     <p>
-//                       {item?.quantity} * ${item?.price} ={" "}
-//                       <b>${(item.quantity * item.price).toFixed(2)}</b>
-//                     </p>
-//                   </div>
-//                 </div>
-//               </div>
-//               <hr />
-//             </>
-//           ))}
-//         </div>
-
-//         <div className="col-12 col-lg-3 my-4">
-//           <div id="order_summary">
-//             <h4>Order Summary</h4>
-//             <hr />
-//             <p>
-//               Subtotal:{" "}
-//               <span className="order-summary-values">${itemsPrice}</span>
-//             </p>
-//             <p>
-//               Shipping:{" "}
-//               <span className="order-summary-values">${shippingPrice}</span>
-//             </p>
-//             <p>
-//               Tax: <span className="order-summary-values">${taxPrice}</span>
-//             </p>
-
-//             <hr />
-
-//             <p>
-//               Total: <span className="order-summary-values">${totalPrice}</span>
-//             </p>
-
-//             <hr />
-//             <Link
-//               to="/payment_method"
-//               id="checkout_btn"
-//               className="btn btn-primary w-100"
-//             >
-//               Proceed to Payment
-//             </Link>
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default ConfirmOrder;
-
-//-------------------updated -----------------------------------------
 import React from "react";
 import MetaData from "../layout/MetaData";
 import { useSelector } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { calculateOrderCost } from "../../helpers/helpers";
 import CheckoutSteps from "./CheckoutSteps";
-import {
-  useCreateNewOrderMutation,
-  useStripeCheckoutSessionMutation,
-} from "../../redux/api/orderApi";
-import { useDispatch } from "react-redux";
-import { clearCart } from "../../redux/features/cartSlice";
-import { toast } from "react-hot-toast";
 
 const ConfirmOrder = () => {
   const { cartItems, shippingInfo } = useSelector((state) => state.cart);
   const { user } = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+
+  //const navigate = useNavigate();
 
   const { itemsPrice, shippingPrice, taxPrice, totalPrice } =
     calculateOrderCost(cartItems);
 
-  const [createNewOrder] = useCreateNewOrderMutation();
-  const [stripeCheckoutSession] = useStripeCheckoutSessionMutation();
-
-  // Handler for COD order
-  const placeCODOrder = async () => {
-    try {
-      await createNewOrder({
-        orderItems: cartItems,
-        shippingInfo,
-        paymentMethod: "COD",
-        itemsPrice,
-        shippingPrice,
-        taxPrice,
-        totalAmount: totalPrice,
-      }).unwrap();
-
-      dispatch(clearCart());
-      toast.success("Your COD order has been placed successfully!");
-      navigate("/me/orders");
-    } catch (err) {
-      toast.error(err?.data?.message || "Failed to place COD order");
-    }
-  };
-
-  // Handler for Stripe payment
-  const proceedToPayment = async () => {
-    try {
-      const { url } = await stripeCheckoutSession({
-        orderItems: cartItems,
-        shippingInfo,
-        itemsPrice,
-        shippingPrice,
-        taxPrice,
-        totalAmount: totalPrice,
-      }).unwrap();
-
-      // Redirect to Stripe checkout
-      window.location.href = url;
-    } catch (err) {
-      toast.error(err?.data?.message || "Failed to initiate payment");
-    }
-  };
-
+  //   const proceedToPaymentHandler = () => {
+  //     navigate("/payment_method");
+  //   };
   return (
     <>
       <MetaData title={"Confirm Order Info"} />
@@ -190,21 +32,21 @@ const ConfirmOrder = () => {
             <b>Phone:</b> {shippingInfo?.phoneNo}
           </p>
           <p className="mb-4">
-            <b>Address:</b> {shippingInfo?.address}, {shippingInfo?.city},{" "}
+            <b>Address:</b> {shippingInfo?.address}, {shippingInfo?.city},
             {shippingInfo?.zipCode}, {shippingInfo?.country}
           </p>
 
           <hr />
           <h4 className="mt-4">Your Cart Items:</h4>
           {cartItems?.map((item) => (
-            <div key={item.product}>
+            <>
               <hr />
               <div className="cart-item my-1">
                 <div className="row">
                   <div className="col-4 col-lg-2">
                     <img
                       src={item?.image}
-                      alt={item?.name}
+                      alt="Laptop"
                       height="45"
                       width="65"
                     />
@@ -222,7 +64,8 @@ const ConfirmOrder = () => {
                   </div>
                 </div>
               </div>
-            </div>
+              <hr />
+            </>
           ))}
         </div>
 
@@ -241,23 +84,21 @@ const ConfirmOrder = () => {
             <p>
               Tax: <span className="order-summary-values">${taxPrice}</span>
             </p>
+
             <hr />
+
             <p>
               Total: <span className="order-summary-values">${totalPrice}</span>
             </p>
+
             <hr />
-            <button
-              className="btn btn-primary w-100 mb-2"
-              onClick={placeCODOrder}
+            <Link
+              to="/payment_method"
+              id="checkout_btn"
+              className="btn btn-primary w-100"
             >
-              Place COD Order
-            </button>
-            <button
-              className="btn btn-success w-100"
-              onClick={proceedToPayment}
-            >
-              Pay with Card
-            </button>
+              Proceed to Payment
+            </Link>
           </div>
         </div>
       </div>
