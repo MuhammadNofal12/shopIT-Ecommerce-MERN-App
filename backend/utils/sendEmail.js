2133a0508f092fb50dde660d36388bced72d472a
Muhammad Nofal12 <muhammadnofal446@gmail.com>
2026-03-08 16:38:27 +0500

    update sendEmail by mailtrap api

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -23,38 +23,70 @@
 
 // export default sendEmail;
 
-//---------------------------------updated-------------------------
-import nodemailer from "nodemailer";
+//---------------------------------updated------------------------------------
+// import nodemailer from "nodemailer";
+
+// const sendEmail = async (options) => {
+//   // ✅ Create transporter
+//   const transporter = nodemailer.createTransport({
+//     host: process.env.SMTP_HOST,
+//     port: Number(process.env.SMTP_PORT),
+//     secure: false, // ✅ required for port 2525 (Mailtrap)
+//     auth: {
+//       user: process.env.SMTP_EMAIL,
+//       pass: process.env.SMTP_PASSWORD,
+//     },
+//   });
+
+//   // ✅ Verify SMTP connection
+//   await transporter.verify();
+//   console.log("SMTP server connected ✅");
+
+//   // ✅ Email message
+//   const message = {
+//     from: `${process.env.SMTP_FROM_NAME} <${process.env.SMTP_FROM_EMAIL}>`,
+//     to: options.email,
+//     subject: options.subject,
+//     html: options.message,
+//   };
+
+//   // ✅ Send email
+//   const info = await transporter.sendMail(message);
+
+//   console.log("Email sent successfully ✅");
+//   console.log("Message ID:", info.messageId);
+// };
+
+// export default sendEmail;
+
+//----------------------updated---------------------------------------------------------------------
+import axios from "axios";
 
 const sendEmail = async (options) => {
-  // ✅ Create transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT),
-    secure: false, // ✅ required for port 2525 (Mailtrap)
-    auth: {
-      user: process.env.SMTP_EMAIL,
-      pass: process.env.SMTP_PASSWORD,
+  console.log(
+    "MAILTRAP_API_TOKEN:",
+    process.env.MAILTRAP_API_TOKEN ? "Loaded ✅" : "Missing ❌",
+  );
+  await axios.post(
+    "https://sandbox.api.mailtrap.io/api/send/4308263",
+    {
+      from: {
+        email: "noreply@shopit.com",
+        name: "ShopIT",
+      },
+      to: [{ email: options.email }],
+      subject: options.subject,
+      html: options.message,
     },
-  });
-
-  // ✅ Verify SMTP connection
-  await transporter.verify();
-  console.log("SMTP server connected ✅");
-
-  // ✅ Email message
-  const message = {
-    from: `${process.env.SMTP_FROM_NAME} <${process.env.SMTP_FROM_EMAIL}>`,
-    to: options.email,
-    subject: options.subject,
-    html: options.message,
-  };
-
-  // ✅ Send email
-  const info = await transporter.sendMail(message);
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.MAILTRAP_API_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    },
+  );
 
   console.log("Email sent successfully ✅");
-  console.log("Message ID:", info.messageId);
 };
 
 export default sendEmail;
